fd6df9cfe0375025a39f78c1655dbe69be1575f9
Mambamel-dev <57382354+Mambamel-dev@users.noreply.github.com>
2022-09-11 00:03:21 +0800

    Add Skills Component
    Add Skills Component, Styling and Icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
 import ProductList from "./components/productList/ProductList";
 import Contact from "./components/contact/Contact";
+import Skills from "./components/skills/Skills";
 const App = () => {
   return (
     <div>
       <Intro />
       <About />
       <ProductList />
+      <Skills />
       <Contact />
     </div>
   );
---
+++ client/src/components/skills/Skills.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Html from "../../img/html-5.png";
+import Css from "../../img/css-3.png";
+import Js from "../../img/js.png";
+import Bootstrap from "../../img/bootstrap.png";
+import Figma from "../../img/figma.png";
+import ReactJs from "../../img/react.png";
+import Mongodb from "../../img/mongodb.png";
+import Express from "../../img/express.png";
+import Node from "../../img/nodejs.png";
+import "./skills.css";
+const Skills = () => {
+  return (
+    <div className="s">
+      <div className="s-title">My Skills</div>
+
+      <div className="s-wrapper">
+        <img src={Html} alt="" className="s-icon" />
+        <img src={Css} alt="" className="s-icon" />
+        <img src={Js} alt="" className="s-icon" />
+        <img src={Bootstrap} alt="" className="s-icon" />
+        <img src={Figma} alt="" className="s-icon" />
+        <img src={ReactJs} alt="" className="s-icon" />
+        <img src={Mongodb} alt="" className="s-icon" />
+        <img src={Express} alt="" className="s-icon" />
+        <img src={Node} alt="" className="s-icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
