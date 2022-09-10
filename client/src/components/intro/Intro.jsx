421d74439875b0ac213fe386ff9d7fadebc0a5b6
Mambamel-dev <57382354+Mambamel-dev@users.noreply.github.com>
2022-09-10 14:53:48 +0800

    Update Intro.jsx

--- client/src/components/intro/Intro.jsx
+++ client/src/components/intro/Intro.jsx
@@ -6,7 +6,7 @@ const Intro = () => {
     <div className="i">
       <div className="i-left">
         <div className="i-left-wrapper">
-          <h2 className="i-intro">Hello My name is</h2>
+          <h2 className="i-intro">Hello my name is</h2>
           <h1 className="i-name">Mel Espiloy</h1>
           <div className="i-title">
             <div className="i-title-wrapper">
