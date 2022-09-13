b2949e33a01a0f8c6d7bc0f30bc019f829e6477c
Mambamel-dev <57382354+Mambamel-dev@users.noreply.github.com>
2022-09-13 22:26:48 +0800

    Add Toggle button Component
    Add and Styling the toggle button component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import About from "./components/about/About";
 import ProductList from "./components/productList/ProductList";
 import Contact from "./components/contact/Contact";
 import Skills from "./components/skills/Skills";
+import Toggle from "./components/toggle/Toggle";
 const App = () => {
   return (
     <div>
+      <Toggle />
       <Intro />
       <About />
       <ProductList />
